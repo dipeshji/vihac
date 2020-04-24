aac632431c48466b26fd14a6106cf4b7f6d7ee10
dipeji <dpatidar336@gmail.com>
2020-04-24 19:55:06 +0530

    updates

--- Front/vihaportal/src/app/app-routing.module.ts
+++ Front/vihaportal/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from '../app/initialComponent/home/home.component';
 
 
 const routes: Routes = [
+  { path:'', component: HomeComponent },
   {
     path: 'user',
     loadChildren: () => import('./modules/login-signup/login-signup.module')
--- Front/vihaportal/src/app/app.component.ts
+++ Front/vihaportal/src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'vihaportal';
+  navOpen = false;
+  searchedUser: any;
+  matchedUser = [];
+  match=false;
+
+  fakeUser = {
+    'user1': 'Dipesh',
+    'user2': 'Pesh',
+    'user3': 'Dipak',
+    'user4': 'Pepak',
+    'user5': 'Lucku',
+    'user6': 'Lumpy',
+    'user7': 'Palak',
+    'user8': 'Pachak',
+    'user9': 'Mahesh',
+    'user10': 'Mahak'
+  }
+
+  Users = new Observable(observer => {
+    let value = Object.values(this.fakeUser)
+    value.map((user) => {
+      user.includes(this.searchedUser) ? this.matchedUser.push(user) : null;
+    })
+    return this.matchedUser.length !== null ? observer.next(this.matchedUser) : null;
+  })
+
+  ngOnInit() { }
+
+  toggleNavbar() {
+    this.navOpen = !this.navOpen;
+  }
+  searchUser(user) {
+    this.matchedUser = [];
+    this.Users.subscribe(value => {
+      if (value[0] !== null) this.match = true;
+    });
+  }
 }
